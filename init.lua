f74ede0361dd0c5273659404c37ae8f6c40f99e2
Lutetium-Vanadium <luv.s7000@gmail.com>
2023-01-01 16:48:50 +0530

    Highlight yank

--- init.lua
+++ init.lua
@@ -6,4 +6,23 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Highlight on yank
+local yank_group = augroup('HighlightYank', {})
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 1000 })
+    end,
+})
+
+-- Format on save
+local format_group = augroup('FormatOnSave', {})
+autocmd('BufWritePre', {
+    group = format_group,
+    pattern = '*',
+    callback = vim.lsp.buf.formatting_sync,
+})
